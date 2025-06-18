eaa8ff8558679605307c78602888cc218c498c2c
Jayashree <s.jayashree94@gmail.com>
2025-06-18 06:03:23 +0530

    first commit

---
+++ educity/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from "../../assets/logo.png"
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+ <nav className='container'>
+        <img src={logo} alt="Logo" className='logo'></img>
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li><button className='btn'>Contact Us</button></li>
+        </ul>
+        
+
+    </nav>
+  )
+}
+
+export default Navbar
+
+
+
