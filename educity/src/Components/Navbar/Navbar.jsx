1a07e4c32a91e247dea7ee35fc5449ea2a4a84d2
Jayashree <s.jayashree94@gmail.com>
2025-06-20 05:47:25 +0530

    responsive page in progress

--- educity/src/Components/Navbar/Navbar.jsx
+++ educity/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import logo from "../../assets/logo.png"
+import menu_icon from "../../assets/menu-icon.png"
+
 import "./Navbar.css";
+import {Link} from "react-scroll";
 
 const Navbar = () => {
   const [sticky,setSticky] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMenu = () =>{
+    mobileMenu? setMobileMenu(false) : setMobileMenu(true)
+  }
 
   useEffect(() =>{
     window.addEventListener('scroll', ()=>{
@@ -14,14 +22,15 @@ const Navbar = () => {
   return (
  <nav className={`container ${sticky ? 'dark-navbar' : '' }`}>
         <img src={logo} alt="Logo" className='logo'></img>
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About Us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li><button className='btn'>Contact Us</button></li>
+        <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+            <li><Link to='hero' smooth={true} offset={0} duration={500} >Home</Link></li>
+            <li><Link to='programs' smooth={true} offset={-260} duration={500} >Program</Link></li>
+            <li><Link to='about' smooth={true} offset={-150} duration={500} >About Us</Link></li>
+            <li><Link to='campus' smooth={true} offset={-260} duration={500} >Campus</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-260} duration={500} >Testimonials</Link></li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
         </ul>
+        <img className="menu-icon" src={menu_icon} alt="menu_icon" onClick={toggleMenu}></img>
         
 
     </nav>
